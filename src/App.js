45f86c9ee374f9711fa518d883f4c77129701699
Michael Morgan <morganmichael340@gmail.com>
2021-05-22 12:23:52 -0700

    add project structure
    + adds form elements
    + adds README

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      
+      const user = {
+        firstName: firstName,
+        lastName: lastName,
+        phoneNumber: phoneNumber,
+        email: email,
+        password: password
+      }
+      console.log(JSON.stringify(user, null, 2));
+
+    }
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <h1>Sign Up</h1>
+
+        <label>
+          First Name:
+          <input
+            name="firstName"
+            type="firstName"
+            value={firstName}
+            onChange={e => setFirstName(e.target.value)}
+            required />
+        </label>
+
+        <label>
+          Last Name:
+          <input
+            name="lastName"
+            type="lastName"
+            value={lastName}
+            onChange={e => setLastName(e.target.value)}
+            required />
+        </label>
+
+        <label>
+          Phone Number:
+          <input
+            name="phoneNumber"
+            type="phoneNumber"
+            value={phoneNumber}
+            onChange={e => setPhoneNumber(e.target.value)}
+            required />
+        </label>
+
+        <label>
+          Email:
+          <input
+            name="email"
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required />
+        </label>
+
+        <label>
+          Password:
+          <input
+            name="password"
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            required />
+        </label>
+
+        <button>Submit</button>
+      </form>
+    );
 }
 
 export default App;
